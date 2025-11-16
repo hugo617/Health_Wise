297762c57df9d558974cfa3a8eaaefcc3408a0e8
star <star@starworking.com>
2025-11-16 13:55:37 +0800

    添加侧边栏路由显示,服务层（Services）规范

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :show_sidenav?
+  
+
+  def show_sidenav?
+  # 只在特定的控制器和动作中显示侧边栏
+  (controller_name == 'users' && action_name == 'index') ||
+  (controller_name == 'projects' && action_name.in?(%w[index show]))
+  end
+  
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
